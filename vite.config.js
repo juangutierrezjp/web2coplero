601a18195b7a489f24dae75b539c0b23ee308294
juan gutierrez <ggjuanpatricio@gmail.com>
2026-05-03 15:37:20 -0300

    brandbook

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,21 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/',
   server: {
-    port: 5173,
+    port: 5174,
     open: true,
   },
   build: {
     outDir: 'dist',
     assetsInlineLimit: 4096,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        brandbook: 'brandbook.html',
+      }
+    }
   },
+  // SPA-like fallback for dev server
+  preview: {
+    port: 4173,
+  }
 })
